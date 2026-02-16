d6c05ac1c6c58c5b6fad36126533d5e7d5cd469e
santushtmatra11 <santushtm05@gmail.com>
2026-02-16 22:34:54 +0530

    chore: setup custom userdetailsservice for principal auth entity

---
+++ src/main/java/com/example/trackerbackend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.trackerbackend.service;
+
+import com.example.trackerbackend.DAO.UserDAO;
+import com.example.trackerbackend.entity.User;
+import com.example.trackerbackend.entity.principal.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserDAO userDAO;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userDB = userDAO.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        return new CustomUserDetails(userDB);
+    }
+
+    public UserDetails loadUserById(Integer id) {
+        System.out.println("Loading User for id: "+id);
+        User user = userDAO.findById(id)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+        System.out.println("Got User:"+user+"\n Password:"+user.getPassword());
+        return new CustomUserDetails(user);
+    }
+
+}
