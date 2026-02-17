737db767c8488675a0e0f844ba77ccdd4f24b1e9
santushtmatra11 <santushtm05@gmail.com>
2026-02-17 14:35:34 +0530

    refactor: Book to DTO conversion tags syncing

--- src/main/java/com/example/trackerbackend/utils/conversion/EntityConversionUtils.java
+++ src/main/java/com/example/trackerbackend/utils/conversion/EntityConversionUtils.java
@@ -1,14 +1,28 @@
 package com.example.trackerbackend.utils.conversion;
 
 import com.example.trackerbackend.DTO.response.BookDTO;
+import com.example.trackerbackend.DTO.response.TagDTO;
 import com.example.trackerbackend.DTO.response.UserDTO;
 import com.example.trackerbackend.entity.Book;
+import com.example.trackerbackend.entity.Tag;
 import com.example.trackerbackend.entity.User;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class EntityConversionUtils {
     public static BookDTO toBookDTO(Book book){
         if (book == null) {
             return null;
+        }
+
+        Set<TagDTO> tagDTOs = null;
+
+        if (book.getTags() != null) {
+            tagDTOs = new HashSet<>();
+            for (Tag tag : book.getTags()) {
+                tagDTOs.add(toTagDTO(tag));
+            }
         }
 
         return BookDTO.builder()
@@ -22,6 +36,7 @@ public class EntityConversionUtils {
                 )
                 .rating(book.getRating())
                 .pages(book.getPages())
+                .tags(tagDTOs)
                 .thumbnailUrl(book.getThumbnailUrl())
                 .completedAt(book.getCompletedAt())
                 .build();
@@ -32,6 +47,13 @@ public class EntityConversionUtils {
                 .id(user.getId())
                 .username(user.getUsername())
                 .fullName(user.getFullName())
+                .build();
+    }
+
+    public static TagDTO toTagDTO(Tag tag) {
+        return TagDTO.builder()
+                .id(tag.getId())
+                .tagName(tag.getName())
                 .build();
     }
 }
