b7f1343bc5fc2696b1992567123cd46670809c30
santushtmatra11 <santushtm05@gmail.com>
2026-02-17 18:53:13 +0530

    chore: implemented regex based password validation

--- src/main/java/com/example/trackerbackend/DTO/request/user/UserCreationRequestDTO.java
+++ src/main/java/com/example/trackerbackend/DTO/request/user/UserCreationRequestDTO.java
@@ -3,6 +3,7 @@ package com.example.trackerbackend.DTO.request.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -19,6 +20,10 @@ public class UserCreationRequestDTO {
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()_\\-+=])[A-Za-z\\d@$!%*?&^#()_\\-+=]{8,}$",
+            message = "Password must contain 8+ chars, upper, lower, digit and special character"
+    )
     private String password;
 
     @NotBlank(message = "Full name is required")
